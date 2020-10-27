53a5e6fd7f5471b1a89f21e2588e30c7095629e3
Fiorella Effio <fiorella.effio@unmsm.edu.pe>
2020-10-27 09:54:27 -0500

    [ADD] Config router trips only auth user

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import firebase from 'firebase';
 import Home from '../views/Home.vue';
 import Search from '../views/Search.vue';
 import StayUserList from '../views/StayUserList.vue';
@@ -7,6 +8,10 @@ import StayUserList from '../views/StayUserList.vue';
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '*',
+    redirect: '/',
+  },
   {
     path: '/',
     name: 'Home',
@@ -21,6 +26,9 @@ const routes = [
     path: '/trips',
     name: 'StayUserList',
     component: StayUserList,
+    meta: {
+      userAuth: true,
+    },
   },
 ];
 
@@ -28,6 +36,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const user = firebase.auth().currentUser;
+  const auth = to.matched.some((record) => record.meta.userAuth);
+  if (auth && !user) {
+    next('/');
+  } else if (!auth && user) {
+    next();
+  } else {
+    next();
+  }
 });
 
 export default router;
